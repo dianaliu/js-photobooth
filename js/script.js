ac039c6b95abbf8eb27e265a170f39c06d2b6004
Diana Liu <dianaliu@nyu.edu>
2012-08-28 12:31:32 -0400

    bug fixes to gallery view

--- js/script.js
+++ js/script.js
@@ -11,126 +11,110 @@
 })(jQuery);
 
 
-$(function() {
-	
 
-	navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.getUserMedia;
-	window.URL = window.URL || window.webkitURL;
+// ======================================================================================  
+// 		Backbone Shizz
+//		Placed outside bc of http://ricostacruz.com/backbone-patterns/#abuse
+// ======================================================================================
+
+var Photo = Backbone.Model.extend({
+	src: "http://placehold.it/193x145"
+});
+
+var PhotoCollection = Backbone.Collection.extend({
+	localStorage: new Backbone.LocalStorage("PhotoCollection"),
+	model: Photo
+});
+
+var GalleryItemView = Backbone.View.extend({
+	tagName: "li",
+	events: {
+		"click .hover-delete": "remove",
+		"hover .thumbnail": "toggle"	
+	},
+	initialize: function() {
+		_.bindAll(this, "render", "remove", "unrender", "toggle");
+		this.model.bind("remove", this.unrender); // If the model is deleted, unrender.
+	},
+	render: function() {			
+		$(this.el).html(Mustache.render(gallery_item_template, {src : this.model.get("src")}));
+		$(this.el).addClass("span4");
+		return this; // for method chaining
+	},
+	unrender: function() {
+		$(this.el).remove();	
+	},
+	remove: function() {
+		this.model.destroy(); // triggers unrender
+	},
+	toggle: function() {
+		$(".hover-original", this.el).toggleClass("block");
+		$(".hover-delete", this.el).toggleClass("block");
+	}		
+});
 
 
-	// Doesn't recognize the jquery selected canvas element.
-	var video = document.getElementById("vid");
-	var canvas = document.getElementById("photo");
-	var gallery = $(document.getElementById("gallery-list"));
-	var ctx = canvas.getContext('2d');  
-	var layersContainer;
-	var image;
-	var overlay;
-	var frame_choice = null;
-	var default_overlay_items = ['frame_unicorn.png', 'frame_2.png', 'frame_4.png', 'frame_5.png'];
-	var overlay_item_template = "<li class='span6'><div class='thumbnail'><img src='{{src}}' alt='' /> </div></li>";
-	var gallery_item_template = "<li class='span5'><div class='thumbnail'><img src='{{src}}'><a href='{{src}}' target='_blank'><img src='img/download-white-100-70.png' class='hover hover-original'></a><span><img src='img/remove-50.png' class='hover hover-delete'></span></div></li>";
-	
-	
-	// ==============================  
-	// 		Backbone Shizz
-	// ==============================
-	
-	var Photo = Backbone.Model.extend({
-		src: "http://placehold.it/193x145"
-	});
-	
-	var PhotoCollection = Backbone.Collection.extend({
-		localStorage: new Backbone.LocalStorage("PhotoCollection"),
-		model: Photo
-	});
-	
-	var GalleryItemView = Backbone.View.extend({
-		tagName: "li",
-		events: {
-			"click .hover-delete": "remove",
-			"hover .thumbnail": "toggle"		
-		},
-		initialize: function() {
-			_.bindAll(this, "render", "remove", "unrender", "toggle");
-			this.model.bind("remove", this.unrender); // If the model is deleted, unrender.
-		},
-		render: function() {			
-			$(this.el).html("<div class='thumbnail'><img src='" + this.model.get("src") + "'><a href='" + this.model.get("src") + "' target='_blank'><img src='img/download-white-100-70.png' class='hover hover-original'></a><span><img src='img/remove-50.png' class='hover hover-delete'></span></div>");
+GalleryListView = Backbone.View.extend({
+	el: $("#gallery"),
+	events: { // Selectors only search descendents.
+		"click #capture": "capture",
+		"click #remove": "clear_frames"
+	},
+	initialize: function() {
+		
+		console.log("Init new gallery list view");
+		
+		_.bindAll(this, "render", "appendItem", "capture", "clear_frames"); // Each function that uses this must be bound
+		
+		this.collection = new PhotoCollection();
+		this.collection.bind("add", this.appendItem); // appendItem after adding to collection
+
+		this.collection.fetch();
+
+		this.render();
+	},
+	render: function() {
+		var self = this;
+		
+		_(this.collection.models).each(function(item) {
+			self.appendItem(item);	
+		}, this);
+	},
+	appendItem: function(item) {
+		// Add the picture you took to gallery view		
+		var galleryItemView = new GalleryItemView({
+			model: item,
+			collection: this.collection
+		});
+		
+		$("ul#gallery-list", this.el).append(galleryItemView.render().el);
+	},
+	capture: function(e) {	
+		// Take a picture
+				
+		// Draw the current video to canvas.
+		layersContainer.markRectsDamaged();	
+		layersContainer.redraw();
+		
+		// Make a png from the canvas.
+		var p = new Photo();
+		p.set("src", canvas.toDataURL('image/png'));
+		this.collection.create(p); // create is add and sync.			
+
+
+	},
+	clear_frames: function(e) {	
+		// Clear all frames off the view.
 			
-			$(this.el).addClass("span5");
-			
-			return this; // for method chaining
-		},
-		unrender: function() {
-			$(this.el).remove();	
-		},
-		remove: function() {
-			this.model.destroy(); // triggers unrender
-		},
-		toggle: function(e) {
-			// FIXME: Weird delay and gets stuck.
-			$(".hover-original", e.target).toggleClass("block");
-			$(".hover-delete", e.target).toggleClass("block");
-			console.log("toggle");
-		}		
-	});
-
-
-	GalleryListView = Backbone.View.extend({
-		el: $("div"),
-		id: "gallery",
-		events: { // Selectors only search descendents.
-			"click #capture": "capture",
-			"click #remove": "clear_frames"
-		},
-		initialize: function() {
-			
-			_.bindAll(this, "render", "appendItem", "capture"); // Each function that uses this must be bound
-			
-			this.collection = new PhotoCollection();
-			this.collection.bind("add", this.appendItem); // appendItem after adding to collection
-
-			this.collection.fetch();
-
-			this.render();
-		},
-		render: function() {
-			var self = this;
-			
-			_(this.collection.models).each(function(item) {
-				self.appendItem(item);	
-			}, this);
-		},
-		appendItem: function(item) {		
-			var galleryItemView = new GalleryItemView({
-				model: item,
-				collection: this.collection
-			});
-			
-			$("ul#gallery-list", this.el).append(galleryItemView.render().el);
-		},
-		capture: function() {			
+		if(frame_choice) {
+			frame_choice =  null;
+			$("#canvas-preview").html("");
 			layersContainer.markRectsDamaged();	
 			layersContainer.redraw();
-			var p = new Photo();
-			p.set("src", canvas.toDataURL('image/png'));
-			this.collection.create(p); // create is add and sync.
-	
-			return false;	
-		},
-		clear_frames: function(e) {
-			if(frame_choice) {
-				frame_choice =  null;
-				$("#canvas-preview").html("");
-				layersContainer.markRectsDamaged();	
-				layersContainer.redraw();
-				console.log("clear frames");
-			}
 		}
-	});
+	}
+});
 
-	var galleryListView = new GalleryListView();
 
 	
 	// ==============================  
@@ -150,6 +134,9 @@ $(function() {
 		$.each(default_overlay_items, function(i, item) {
 			$('#frames').append(Mustache.render(overlay_item_template, {src: ("img/" + item	)}));
 		});
+		
+		
+		galleryListView = new GalleryListView();
 		
 		
 	}
@@ -223,7 +210,7 @@ $(function() {
 	// ==============================  
 	// 		Register Events
 	// ============================== 
-	
+
 	$("#frames").on("click", "img", function (e) {
 		frame_choice = e.target;
 		
@@ -232,32 +219,35 @@ $(function() {
 		$("#canvas-preview").html(f);
 	});
 
-	// Rename to "clear".
-	$("#remove").click(function() {
-
-		// Clear out the frame but keep the layer there for drawing.
-		if(frame_choice) {
-			frame_choice =  null;
-			$("#canvas-preview").html("");
-			layersContainer.markRectsDamaged();	
-			layersContainer.redraw();
-		}
-	
-	});
-
 	$("#add-frame").submit(function() {
 		
 		// TODO: Real uploading.
-		var s = $("input").val();
-		var new_img = new Photo();
-		new_img.set("src", s);			
-		var new_img_html = Mustache.render(overlay_item_template, new_img);	
-		$("#frames").append(new_img_html);
+		var s = $("input").val();			
+		$("#frames").append(Mustache.render(overlay_item_template, {src: s}));
 		
 		return false;
 	});
 	
 	
+	navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.getUserMedia;
+	window.URL = window.URL || window.webkitURL;
+
+	// Doesn't recognize the jquery selected canvas element.
+	var video = document.getElementById("vid");
+	var canvas = document.getElementById("photo");
+	var gallery = $(document.getElementById("gallery-list"));
+	var ctx = canvas.getContext('2d');  
+	var layersContainer;
+	var image;
+	var overlay;
+	var frame_choice = null;
+	var default_overlay_items = ['frame_unicorn.png', 'frame_2.png', 'frame_4.png', 'frame_5.png'];
+	var overlay_item_template = "<li class='span6'><div class='thumbnail'><img src='{{src}}' alt='' /> </div></li>";
+	var gallery_item_template = "<div class='thumbnail'><img src='{{src}}'><a href='{{src}}' target='_blank'><img src='img/download-white-100-70.png' class='hover hover-original'></a><div><img src='img/remove-50.png' class='hover hover-delete'></div></div>";
+	var galleryListView;	
+
+$(function() {	
+
 	init();
 });
 
