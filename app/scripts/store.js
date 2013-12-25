1b5e50e22e036a6227c8f6cb69fa46969d1b4ff0
dianaliu <diana.g.liu@gmail.com>
2013-12-24 22:11:13 -0500

    Conditionally show delete all button, update copy

--- app/scripts/store.js
+++ app/scripts/store.js
@@ -1,9 +1,4 @@
 JsPhotobooth.Store = DS.Store.extend();
 JsPhotobooth.ApplicationAdapter = DS.LSAdapter.extend({
-  namespace: 'js-photobooth'
-});
-
-// FIXME: this doesn't work
-JsPhotobooth.ApplicationAdapter.on('QUOTA_EXCEEDED_ERR', function(records){
-  Ember.$('#error').text('No more space in localStorage');
+  namespace: 'horcrux'
 });
