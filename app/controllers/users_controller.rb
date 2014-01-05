6b43b1ed9b1f49a676f22804246811ad4e8888f6
dianaliu <diana.g.liu@gmail.com>
2014-01-05 16:32:14 -0500

    Can create a new user :-) Rails 4 does params differently

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+
+  def create
+    @user = User.create(user_params)
+    render json: { :users => @user } if request.xhr?
+  end
+
   def index
     @user = current_user || User.new
     render json: { :users => @user } if request.xhr?
@@ -7,5 +13,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id]) || User.new()
     render json: { :users => @user } if request.xhr?
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
   end
 end
