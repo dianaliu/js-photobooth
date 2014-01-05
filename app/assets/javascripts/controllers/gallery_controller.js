b4cbbe1fd2fdd11d00a15cc5367e6d372c08ea5c
dianaliu <diana.g.liu@gmail.com>
2014-01-05 17:15:11 -0500

    Clear gallery

--- app/assets/javascripts/controllers/gallery_controller.js
+++ app/assets/javascripts/controllers/gallery_controller.js
@@ -9,38 +9,10 @@ Photobooth.GalleryController = Ember.ArrayController.extend({
     clearGallery: function() {
       console.log('clearGallery');
 
-      // works, but doesn't save
-      // model and content are the same thing
+      // TODO: Detect if user is saved to DB
+      // If yes, need to save.
 
-      // FIXME: A proper solution
-      // haha or maybe forget localStorage and do it snapchat style?
       this.get('model').clear();
-      localStorage.clear();
-
-      // this.get('model').invoke('save');
-
-      // only removes one at a time and doesn't save
-      // var photos = this.get('content');
-      // photos.invoke('deleteRecord').invoke('save');
-
-
-      // does nothing at all
-      // photos.invoke('destroyRecord');
-
-      // this.get('model').forEach(function(photo) {
-      //   photo.deleteRecord();
-      //   photo.save();
-      // });
-
-
-
-      // per doc, but queries twice?
-      // Cannot call method 'lookup' of undefined
-      // all returns a live filter
-      // var photos = this.get('model').all('photo');
-      // this.get('model').invoke('save');
-      // photos.invoke('deleteRecord');
-      // photos.invoke('save');
     }
   }
 });
